a650089c1bb1c909832bb88bd7a16425a28f079d
Luiz Filho <luizbafilho@gmail.com>
2016-01-10 22:06:54 -0200

    fixing features request msg version

--- lib/messages/features_request.ex
+++ lib/messages/features_request.ex
@@ -1,7 +1,7 @@
 defmodule OfProto.Messages.FeaturesRequest do
   import OfProto.Constants
-  
-  defstruct version: 1, type: to_index(ofp_type, :OFPT_FEATURES_REQUEST), length: 8, xid: 0
+
+  defstruct version: 4, type: to_index(ofp_type, :OFPT_FEATURES_REQUEST), length: 8, xid: 0
 
   def encode(%OfProto.Messages.FeaturesRequest{version: version, type: type, length: length, xid: xid}) do
     <<version::size(8), type::size(8), length::size(16), xid::size(32)>>
