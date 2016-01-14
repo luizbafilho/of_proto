27579df5de1e6a7384b659082b78583737677910
Luiz Filho <luizbafilho@gmail.com>
2016-01-14 21:15:14 -0200

    adding delegates

--- lib/of_proto.ex
+++ lib/of_proto.ex
@@ -1,13 +1,4 @@
 defmodule OfProto do
-  import OfProto.Constants
-  alias OfProto.Decoder
-  alias OfProto.Encoder
-
-  def encode(message) do
-    Encoder.encode(message)
-  end
-
-  def decode(bin) do
-    Decoder.decode(bin)
-  end
+  defdelegate encode(message), to: OfProto.Encoder
+  defdelegate decode(bin),     to: OfProto.Decoder
 end
